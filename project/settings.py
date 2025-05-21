82971cb5082d4ba774e599c63ff839a011ffdb7b
Nozima <abduazizovanozima7@gmail.com>
2025-05-21 12:09:05 +0500

    news

--- project/settings.py
+++ project/settings.py
@@ -15,8 +15,13 @@ import environ, os
 from celery.schedules import crontab
 
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATIC_ROOT = str(STATIC_ROOT)
+
 
 env = environ.Env()
 env.read_env(os.path.join(os.path.dirname(__file__), '..', '.env'))
@@ -154,8 +159,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
